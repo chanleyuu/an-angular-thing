5e977c47cf2f647545decf3aa9aa531d9d76f85f
chanleyuu <hanley-c3@ulster.ac.uk>
2020-08-06 14:35:09 +0100

    More additions to form2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { RatingsPageComponent } from './components/pages/ratings-page/ratings-page.component';
 import { RatingsFormComponent } from './components/ratings-form/ratings-form.component';
 import { Form2Component} from './components/form2/form2.component';
+import { Form2pageComponent } from './components/pages/form2page/form2page.component';
 
 
 const routes: Routes = [
   { path: '', component: TosComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'ratings-page', component: RatingsPageComponent }
+  { path: 'ratings-page', component: RatingsPageComponent },
+  { path: 'form2page', component: Form2pageComponent }
 ];
 
 @NgModule({
